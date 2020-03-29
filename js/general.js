872f107623262f1c21184ac00cbbdf43bd2b1f21
Vít Jašek <jasekvit@gmail.com>
2020-03-29 17:43:46 +0200

    Příprava ze clientsidu

---
+++ js/general.js
@@ -0,0 +1,8 @@
+function showBurgerMenu() {
+  var x = document.getElementById("mobile_menu");
+  if(x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
---
+++ js/login.js
@@ -0,0 +1,1 @@
+var btn = document.getElementById("js_login");
