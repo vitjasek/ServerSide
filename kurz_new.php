af83f16db36f4b7b8f56ec18557e8a16be623fc5
Vít Jašek <jasekvit@gmail.com>
2020-04-05 15:06:51 +0200

    upraven nový kurz

--- kurz_new.php
+++ kurz_new.php
@@ -5,75 +5,21 @@
 
   if($_SERVER['REQUEST_METHOD']=="POST")
   {
-    $sql = "SELECT id FROM uzivatel WHERE id='{$_SESSION['id']}'";
-    $result = $conn->query($sql);
-    $rowuser = $result->fetch_assoc();
     if(strlen($_POST['name'])>0 && is_uploaded_file($_FILES['pic01']['tmp_name']) && is_uploaded_file($_FILES['pic02']['tmp_name']) && is_uploaded_file($_FILES['pic03']['tmp_name']) && strlen($_POST['anslist01'])>0 && strlen($_POST['anslist02'])>0 && strlen($_POST['anslist03'])>0)
     {
-      $inskurz = "INSERT INTO kurz (id,nazev,userid) VALUES('', '{$_POST['name']}', '{$rowuser['id']}')";
-      $kurzqry = mysqli_query($conn, $inskurz);
-      $selectlast = "SELECT id FROM kurz ORDER BY id DESC LIMIT 1";
-      $lastkurzqry = $conn->query($selectlast);
-      $lastkurzid = $lastkurzqry->fetch_assoc();
+      $inskurz = "INSERT INTO kurz (nazev,userid) VALUES('{$_POST['name']}', '{$_SESSION['id']}')";
 
-      if (is_uploaded_file($_FILES['pic01']['tmp_name']))
-      {
-        $pic = addslashes(file_get_contents($_FILES['pic01']['tmp_name']));
-        $inspic = "INSERT INTO obrazek (id,obrazek) VALUES('', '$pic')";
-        $picqry = mysqli_query($conn, $inspic);
-        $lastpic = "SELECT id FROM obrazek ORDER BY id DESC LIMIT 1";
-        $lastpicqry = $conn->query($lastpic);
-        $lastpicid = $lastpicqry->fetch_assoc();
-        if($_POST['anslist01']=="ano")
-        {
-          $ans = true;
-        }
-        else
-        {
-          $ans = false;
-        }
-        $otazkains = "INSERT INTO otazka (id,datum,kurzid,obrazekid,odpoved) VALUES('', NOW(), '{$lastkurzid['id']}', '{$lastpicid['id']}', '$ans')";
-        $otazkaqry = mysqli_query($conn, $otazkains);
-      }
+      $conn->query($inskurz);
+      $kurz_id = $conn->insert_id;
 
-      if (is_uploaded_file($_FILES['pic02']['tmp_name']))
-      {
-        $pic = addslashes(file_get_contents($_FILES['pic02']['tmp_name']));
-        $inspic = "INSERT INTO obrazek (id,obrazek) VALUES('', '$pic')";
-        $picqry = mysqli_query($conn, $inspic);
-        $lastpic = "SELECT id FROM obrazek ORDER BY id DESC LIMIT 1";
-        $lastpicqry = $conn->query($lastpic);
-        $lastpicid = $lastpicqry->fetch_assoc();
-        if($_POST['anslist02']=="ano")
-        {
-          $ans = true;
-        }
-        else
-        {
-          $ans = false;
-        }
-        $otazkains = "INSERT INTO otazka (id,datum,kurzid,obrazekid,odpoved) VALUES('', NOW(), '{$lastkurzid['id']}', '{$lastpicid['id']}', '$ans')";
-        $otazkaqry = mysqli_query($conn, $otazkains);
-      }
-
-      if (is_uploaded_file($_FILES['pic03']['tmp_name']))
-      {
-        $pic = addslashes(file_get_contents($_FILES['pic03']['tmp_name']));
-        $inspic = "INSERT INTO obrazek (id,obrazek) VALUES('', '$pic')";
-        $picqry = mysqli_query($conn, $inspic);
-        $lastpic = "SELECT id FROM obrazek ORDER BY id DESC LIMIT 1";
-        $lastpicqry = $conn->query($lastpic);
-        $lastpicid = $lastpicqry->fetch_assoc();
-        if($_POST['anslist03']=="ano")
-        {
-          $ans = true;
-        }
-        else
-        {
-          $ans = false;
-        }
-        $otazkains = "INSERT INTO otazka (id,datum,kurzid,obrazekid,odpoved) VALUES('', NOW(), '{$lastkurzid['id']}', '{$lastpicid['id']}', '$ans')";
-        $otazkaqry = mysqli_query($conn, $otazkains);
+      for($i=1; $i<=3; $i++){
+        $pic = $_FILES['pic0'.$i]['tmp_name'];// addslashes(file_get_contents($_FILES['pic0'.$i]['tmp_name']));
+        $inspic = "INSERT INTO obrazek (obrazek) VALUES('$pic')";
+        $conn->query($inspic);
+        $pic_id = $conn->insert_id;
+        $ans = $_POST['anslist0'.$i]=="ano" ? true : false;
+        $otazkains = "INSERT INTO otazka (datum,kurzid,obrazekid,odpoved) VALUES( NOW(), '{$kurz_id}', '{$pic_id}', '$ans')";
+        $conn->query($otazkains);
       }
     }
   }
