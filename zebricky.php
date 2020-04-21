45b1183b8359daf8f85fc72070946c2099a4ebdf
unknown <john.hammer136@gmail.com>
2020-04-21 19:44:34 +0200

    snad už hotovo

--- zebricky.php
+++ zebricky.php
@@ -38,48 +38,62 @@
                 if ($row['id']==$_SESSION['id']) {
                   $us=$i;
                 }
-                
-                if ($i == 1) {
-                 echo '
-                      <div class="tops_wrap"> 
-                        <div class="top second">
+                echo '<div class="tops_wrap">';
+                if ($result->num_rows == 1) {
+                  echo '
+                        <div class="top first">
                         <h3>1#</h3>
                         <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
                         <h3>' . $row['login'] . '</h3>
                         <p>' . $row['skore'] . '</p>
-                      </div>';
-                }
-
-                if ($i == 2) {
-                  echo '<div class="top first">
-                          <h3>2#</h3>
-                          <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
-                          <h3>' . $row['login'] . '</h3>
-                          <p>' . $row['skore'] . '</p>
-                        </div>';
-                }
-
-                if ($i == 3) {
-                  echo '<div class="top third">
-                          <h3>3#</h3>
-                          <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
-                          <h3>' . $row['login'] . '</h3>
-                          <p>' . $row['skore'] . '</p>
                         </div>
                         </div>';
                 }
-                if ($i > 3) {
-                  echo '<div class="places_view">
-                        <div class="place">
-                          <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
-                          <div>' . $row['login'] . '</div>
-                          <span class="score">' . $row['skore'] . '</span>
-                          <span class="score_place" >#'.$i.'</span>
-                        </div>';
+                else {
+                  if ($i == 1) {
+                    $obh = $row['obrazek'];
+                    $logh = $row['login'];
+                    $skh = $row['skore'];
+                   
+                  }
+  
+                  if ($i == 2) {
+                    echo '<div class="top second">
+                            <h3>2#</h3>
+                            <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                            <h3>' . $row['login'] . '</h3>
+                            <p>' . $row['skore'] . '</p>
+                          </div>';
+                    echo '<div class="top first">
+                        <h3>1#</h3>
+                        <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $obh ).'">
+                        <h3>' . $logh . '</h3>
+                        <p>' . $skh . '</p>
+                      </div>';
+                  }
+  
+                  if ($i == 3) {
+                    echo '<div class="top third">
+                            <h3>3#</h3>
+                            <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                            <h3>' . $row['login'] . '</h3>
+                            <p>' . $row['skore'] . '</p>
+                          </div>
+                          </div>';
+                  }
+                  if ($i > 3) {
+                    echo '<div class="places_view">
+                            <div class="place">
+                              <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                              <div>' . $row['login'] . '</div>
+                              <span class="score">' . $row['skore'] . '</span>
+                              <span class="score_place" >#'.$i.'</span>
+                            </div>';
+                  }
+                  
                 }
-                
-
               }
+              echo '</div>';
               $sql = "SELECT uz.login, ob.obrazek, SUM(dok.skore) AS skore, uz.id FROM uzivatel AS uz INNER JOIN dokonceno AS dok ON uz.id=dok.userid LEFT JOIN obrazek AS ob ON uz.obrazekid=ob.id WHERE uz.id='{$_SESSION['id']}' GROUP BY uz.login, ob.obrazek, uz.id ORDER BY skore DESC";
                      $result = mysqli_query($conn, $sql);
                      $resultCheck = mysqli_num_rows($result);
@@ -92,14 +106,13 @@
                           <div class="place">
                                 <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
                             <div>' . $row['login'] . '</div>
-                            <span class="score">' . $row['skore'] . '</span>
+                            <span class="score">Skóre: ' . $row['skore'] . '</span>
                             <span class="score_place" >VAŠE POZICE #' . $us . '</span>
                           </div>
                         </div>
                         <hr class="second_hr">';
                      }
             }
-            echo '</div>';
             echo "</div>";  
              ?>        
 
@@ -120,48 +133,62 @@
                 if ($row['id']==$_SESSION['id']) {
                   $us=$i;
                 }
-                
-                if ($i == 1) {
-                 echo '
-                      <div class="tops_wrap"> 
+                echo '<div class="tops_wrap">';
+                if ($result->num_rows = 1) {
+                  echo '
                         <div class="top first">
                         <h3>1#</h3>
                         <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
                         <h3>' . $row['login'] . '</h3>
                         <p>' . $row['skore'] . '</p>
-                      </div>';
-                }
-
-                if ($i == 2) {
-                  echo '<div class="top second">
-                          <h3>2#</h3>
-                          <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
-                          <h3>' . $row['login'] . '</h3>
-                          <p>' . $row['skore'] . '</p>
-                        </div>';
-                }
-
-                if ($i == 3) {
-                  echo '<div class="top third">
-                          <h3>3#</h3>
-                          <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
-                          <h3>' . $row['login'] . '</h3>
-                          <p>' . $row['skore'] . '</p>
                         </div>
                         </div>';
                 }
-                if ($i > 3) {
-                  echo '<div class="places_view">
-                          <div class="place">
+                else {
+                  if ($i == 1) {
+                    $obh = $row['obrazek'];
+                    $logh = $row['login'];
+                    $skh = $row['skore'];
+                   
+                  }
+  
+                  if ($i == 2) {
+                    echo '<div class="top second">
+                            <h3>2#</h3>
                             <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
-                            <div>' . $row['login'] . '</div>
-                            <span class="score">' . $row['skore'] . '</span>
-                            <span class="score_place" >#'.$i.'</span>
+                            <h3>' . $row['login'] . '</h3>
+                            <p>' . $row['skore'] . '</p>
                           </div>';
+                    echo '<div class="top first">
+                        <h3>1#</h3>
+                        <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $obh ).'">
+                        <h3>' . $logh . '</h3>
+                        <p>' . $skh . '</p>
+                      </div>';
+                  }
+  
+                  if ($i == 3) {
+                    echo '<div class="top third">
+                            <h3>3#</h3>
+                            <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                            <h3>' . $row['login'] . '</h3>
+                            <p>' . $row['skore'] . '</p>
+                          </div>
+                          </div>';
+                  }
+                  if ($i > 3) {
+                    echo '<div class="places_view">
+                            <div class="place">
+                              <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                              <div>' . $row['login'] . '</div>
+                              <span class="score">Skóre: ' . $row['skore'] . '</span>
+                              <span class="score_place" >#'.$i.'</span>
+                            </div>';
+                  }
+                  
+  
                 }
-                
-
-              }
+                echo '</div>';
                  $sql = "SELECT uz.login, ob.obrazek, SUM(dok.skore) AS skore, uz.id FROM uzivatel AS uz INNER JOIN dokonceno AS dok ON uz.id=dok.userid LEFT JOIN obrazek AS ob ON uz.obrazekid=ob.id WHERE uz.id='{$_SESSION['id']}' GROUP BY uz.login, ob.obrazek, uz.id ORDER BY skore DESC LIMIT 1";
                      $result = mysqli_query($conn, $sql);
                      $resultCheck = mysqli_num_rows($result);
@@ -180,8 +207,8 @@
                         </div>
                         <hr class="second_hr">';
                      }
+              }
             }
-            echo '</div>';
             echo "</div>";  
              ?>        
       </div>
