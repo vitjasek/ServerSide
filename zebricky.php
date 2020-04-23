ff38540da0a51f1e9f11e09839775a8eff4b8bdc
unknown <john.hammer136@gmail.com>
2020-04-23 13:46:15 +0200

    fix

--- zebricky.php
+++ zebricky.php
@@ -19,13 +19,6 @@
         <h1>Žebříčky</h1>
        
         <?php
-/*        $sqlCreateView = "CREATE VIEW skoreview AS SELECT uzivatel.login, obrazek.obrazek, SUM(dokonceno.skore) AS skore, ROW_NUMBER() OVER(ORDER BY skore DESC) AS num, uzivatel.id FROM uzivatel, dokonceno, obrazek WHERE uzivatel.id = dokonceno.userid && uzivatel.obrazekid = obrazek.id GROUP BY login ORDER BY skore DESC";
-        $sqlCheckView = "SELECT * FROM skoreview LIMIT 1";
-        if (mysqli_query($conn, $sqlCheckView) == false){
-          mysqli_query($conn, $sqlCreateView);
-        }                                            */
-
-
             $sql = "SELECT uz.login, ob.obrazek, SUM(dok.skore) AS skore, uz.id FROM uzivatel AS uz INNER JOIN dokonceno AS dok ON uz.id=dok.userid LEFT JOIN obrazek AS ob ON uz.obrazekid=ob.id GROUP BY uz.login, ob.obrazek, uz.id ORDER BY skore DESC";
             $result = mysqli_query($conn, $sql);
             $resultCheck = mysqli_num_rows($result);
@@ -38,9 +31,8 @@
                 if ($row['id']==$_SESSION['id']) {
                   $us=$i;
                 }
-                echo '<div class="tops_wrap">';
                 if ($result->num_rows == 1) {
-                  echo '
+                  echo '<div class="tops_wrap">
                         <div class="top first">
                         <h3>1#</h3>
                         <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
@@ -58,7 +50,8 @@
                   }
   
                   if ($i == 2) {
-                    echo '<div class="top second">
+                    echo '<div class="tops_wrap">
+                          <div class="top second">
                             <h3>2#</h3>
                             <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
                             <h3>' . $row['login'] . '</h3>
@@ -81,7 +74,7 @@
                           </div>
                           </div>';
                   }
-                  if ($i > 3) {
+                  if ($i == 4) {
                     echo '<div class="places_view">
                             <div class="place">
                               <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
@@ -90,7 +83,14 @@
                               <span class="score_place" >#'.$i.'</span>
                             </div>';
                   }
-                  
+                  if ($i > 4) {
+                    echo '<div class="place">
+                              <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                              <div>' . $row['login'] . '</div>
+                              <span class="score">' . $row['skore'] . '</span>
+                              <span class="score_place" >#'.$i.'</span>
+                            </div>';                  
+                  }                 
                 }
               }
               echo '</div>';
@@ -110,10 +110,11 @@
                             <span class="score_place" >VAŠE POZICE #' . $us . '</span>
                           </div>
                         </div>
-                        <hr class="second_hr">';
-                     }
+                        <hr class="second_hr">
+                        </div>';
+                     }    
             }
-            echo "</div>";  
+
              ?>        
 
       </div>
@@ -144,6 +145,16 @@
                         </div>
                         </div>';
                 }
+                if ($result->num_rows == 1) {
+                  echo '<div class="tops_wrap">
+                        <div class="top first">
+                        <h3>1#</h3>
+                        <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                        <h3>' . $row['login'] . '</h3>
+                        <p>' . $row['skore'] . '</p>
+                        </div>
+                        </div>';
+                }
                 else {
                   if ($i == 1) {
                     $obh = $row['obrazek'];
@@ -153,7 +164,8 @@
                   }
   
                   if ($i == 2) {
-                    echo '<div class="top second">
+                    echo '<div class="tops_wrap">
+                          <div class="top second">
                             <h3>2#</h3>
                             <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
                             <h3>' . $row['login'] . '</h3>
@@ -176,17 +188,23 @@
                           </div>
                           </div>';
                   }
-                  if ($i > 3) {
+                  if ($i == 4) {
                     echo '<div class="places_view">
                             <div class="place">
                               <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
                               <div>' . $row['login'] . '</div>
-                              <span class="score">Skóre: ' . $row['skore'] . '</span>
+                              <span class="score">' . $row['skore'] . '</span>
                               <span class="score_place" >#'.$i.'</span>
                             </div>';
                   }
-                  
-  
+                  if ($i > 4) {
+                    echo '<div class="place">
+                              <img alt = "profilovy obrazek" src="data:image/jpeg;base64,'.base64_encode( $row['obrazek'] ).'">
+                              <div>' . $row['login'] . '</div>
+                              <span class="score">' . $row['skore'] . '</span>
+                              <span class="score_place" >#'.$i.'</span>
+                            </div>';                  
+                  }                 
                 }
                 echo '</div>';
                  $sql = "SELECT uz.login, ob.obrazek, SUM(dok.skore) AS skore, uz.id FROM uzivatel AS uz INNER JOIN dokonceno AS dok ON uz.id=dok.userid LEFT JOIN obrazek AS ob ON uz.obrazekid=ob.id WHERE uz.id='{$_SESSION['id']}' GROUP BY uz.login, ob.obrazek, uz.id ORDER BY skore DESC LIMIT 1";
@@ -205,11 +223,12 @@
                             <span class="score_place" >#' . $us . '</span>
                           </div>
                         </div>
+                        </div>
                         <hr class="second_hr">';
                      }
               }
             }
-            echo "</div>";  
+
              ?>        
       </div>
     </section>
